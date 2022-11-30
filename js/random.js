5e3b8b86e0d2a8ce9f38bb4e74a95ce892ec665f
Pauline Martineau <pamartin@fu-r6-p8.s19.be>
2022-11-30 18:38:15 +0100

    fix random page whitout refresh

--- js/random.js
+++ js/random.js
@@ -1,17 +1,6 @@
-var a = Math.round(Math.random()*105);
-
-let ulField = document.getElementById('aleatoir');
-ulField.innerHTML = ulField.innerHTML +  
-`<a href="./recettes/recette_${a}.html" class="card-2">
-<div class="card">
-    <div class="img-arrow">
-        <img src="./svg/up-arrow.svg">
-    </div>
-    <div class="card_image">
-        <img src="./svg/hasard.svg">
-    </div>
-    <div class="card_title">
-        <p>Recette<br>aléatoire</p>
-    </div>
-</div>
-</a>`;
+function    getRandomPage(){
+    let bouton = document.getElementById("random");
+    var a = Math.round(Math.random()*105);
+    var link = "./recettes/recette_" + a + ".html";
+    bouton.href = link;
+}
